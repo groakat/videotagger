1426157b7acb7421d2b6da89f93e26c02889cb0f
Peter Rennert <p.rennert@cs.ucl.ac.uk>
2013-03-05 16:23:35 +0000

    backgroundModel: estimation from median implemented in pure numpy (fast)

--- videoProc/backgroundModel.py
+++ videoProc/backgroundModel.py
@@ -4,6 +4,7 @@ import datetime as dt
 import random
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib as mpl
 import warnings
 
 class backgroundModel(object):
@@ -50,42 +51,21 @@ class backgroundModel(object):
             warnings.warn("createModelFromListMedian: sampleSize has to be between 0..255", RuntimeWarning)
             sampleSize = 255
         
-        refFrame = np.float32(self.getRandomFrame(pathList))
+        refFrame = self.getRandomFrame(pathList)
         
-        medianSumTbl = np.zeros([refFrame.shape[0], 
-                           refFrame.shape[1], 
-                           255],  dtype=np.uint8)
+        frameBuffer = np.empty([refFrame.shape[0],  
+                                refFrame.shape[1],  sampleSize],
+                                dtype=np.uint8)
+                                
+        frameBuffer[:, :, 0] = refFrame
         
-        for i in range(sampleSize):
+        for i in range(1, sampleSize):
             frame = self.getRandomFrame(pathList)
-            
-            for k in range(medianSumTbl.shape[0]):
-                for m in range(medianSumTbl.shape[1]):
-                    medianSumTbl[k, m, frame[k, m]] += 1
-                    
-        ## accumulate mean values and calculate expected mean value
-        valueMean = np.zeros(refFrame.shape)
-        for i in range(255):
-            valueMean += medianSumTbl[:, :, i] * i
-            
-        valueMean /= 2
+            frameBuffer[:, :, i] = frame
         
-        out = np.zeros(refFrame.shape,  dtype=np.uint8)        
+        medianImage = np.median(frameBuffer, axis=2)
         
-        ## find value that represents the median
-        for k in range(medianSumTbl.shape[0]):
-            for m in range(medianSumTbl.shape[1]):                
-                if self.verbose:
-                    print "determining median of pixel {0} {1}".format(k, m)
-                
-                accValue = 0
-                for i in range(255):
-                    accValue += medianSumTbl[k, m, i]
-                    if accValue >= valueMean[k, m] / 2:
-                        out[k, m] = i
-                        break;
-            
-        return out
+        return medianImage
         
         
     def getRandomFrame(self, pathList):
@@ -100,29 +80,37 @@ class backgroundModel(object):
         return vs.next().ndarray()
                 
     def createNightModel(self, sampleSize=200):
+        if self.verbose:
+            print "creating night model.."
         self.modelNight = self.createModelFromListMedian(self.nightPaths, sampleSize)
         
     def createDayModel(self, sampleSize=200):
+        if self.verbose:
+            print "creating day model.."
         self.modelDay = self.createModelFromListMedian(self.dayPaths,  sampleSize)
 
 if __name__ == "__main__":
     bgModel = backgroundModel(verbose=True)
     
     start = dt.datetime(2013, 02, 19)
-    end = dt.datetime(2013, 02, 20)
+    end = dt.datetime(2013, 02, 21)
+    root = "/run/media/peter/Elements/peter/data/"
     
-    bgModel.getVideoPaths("/run/media/peter/Elements/peter/data/",  start,  end)
+    bgModel.getVideoPaths(root,  start,  end)
     
-    bgModel.createDayModel(3)
-    
-#    bgModel.createNightModel()
+    bgModel.createDayModel(sampleSize=20)    
+    bgModel.createNightModel(sampleSize=20)
     
     plt.imshow(bgModel.modelDay)
     plt.show()
     
-#    plt.imshow(bgModel.modelNight)
+    plt.imshow(bgModel.modelNight)
     plt.show()
     
-    
+    plt.imsave(fname=root + "dayModel_" + start.isoformat() + "--" + end.isoformat(), 
+               arr=bgModel.modelDay, cmap=mpl.cm.gray)
+               
+    plt.imsave(fname=root + "nightModel_" + start.isoformat() + "--" + end.isoformat(), 
+               arr=bgModel.modelNight,  cmap=mpl.cm.gray)
     
     
