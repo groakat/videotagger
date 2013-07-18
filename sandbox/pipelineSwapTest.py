8dcd0b2c07c8670fe6e0b98ad640a90777870e6b
Peter Rennert <p.rennert@cs.ucl.ac.uk>
2013-07-18 13:22:22 +0100

    pipeline swapping using a video file works very well, still no waiting for key frames

--- sandbox/pipelineSwapTest.py
+++ sandbox/pipelineSwapTest.py
@@ -85,16 +85,16 @@ class pipelineSwap(object):
         self.elements["fileQueue1"] = Gst.ElementFactory.make( "queue", "fileQueue1")
         self.elements["ph264_1"] = Gst.ElementFactory.make ("h264parse", "ph264_1")
         self.elements["mux_1"] = Gst.ElementFactory.make("mp4mux", "mux_1")
-        self.elements["filesink1"] = Gst.ElementFactory.make( "filesink", "file_sink1")
+        self.elements["filesink1"] = Gst.ElementFactory.make( "filesink", "filesink")
         
         self.elements["fakesrc"] = Gst.ElementFactory.make( "fakesrc", "fakesrc")
-        self.elements["fakesink"] = Gst.ElementFactory.make( "fakesink", "fakesink")
+#         self.elements["fakesink"] = Gst.ElementFactory.make( "fakesink", "fakesink")
         
         self.elements["recBin2"] = Gst.Bin.new("recoding bin 2")
         self.elements["fileQueue2"] = Gst.ElementFactory.make( "queue", "fileQueue2")
         self.elements["ph264_2"] = Gst.ElementFactory.make ("h264parse", "ph264_2")
         self.elements["mux_2"] = Gst.ElementFactory.make("mp4mux", "mux_2")
-        self.elements["filesink2"] = Gst.ElementFactory.make("filesink", "file_sink2")
+        self.elements["filesink2"] = Gst.ElementFactory.make("filesink", "filesink")
 
 
 
@@ -147,39 +147,44 @@ class pipelineSwap(object):
         self.pads["tPad2"].link(self.elements["srcQueue"].get_static_pad("sink"))  
         self.elements["srcQueue"].link(self.elements["recBin1"])  
         
-        self.pipelines["catch"].add(self.elements["fakesrc"])
-        self.pipelines["catch"].add(self.elements["fakesink"])
-        assert(self.elements["fakesrc"].link(self.elements["fakesink"]))
+#         self.pipelines["catch"].add(self.elements["fakesink"])
+#         assert(self.elements["fakesrc"].link(self.elements["fakesink"]))
+#         
         
-#         self.pipelines["catch"].add(self.elements["fileQueue2"])
-#         self.pipelines["catch"].add(self.elements["ph264_2"])
-#         self.pipelines["catch"].add(self.elements["mux_2"])
-#         self.pipelines["catch"].add(self.elements["filesink2"])
         
-                
-#         tPad2 = Gst.Element.get_request_pad(self.elements["t"], 'src_%u')
-#         tPad2.link(self.elements["fileQueue1"].get_static_pad("sink"))  
-#         assert(self.elements["fileQueue1"].link(self.elements["ph264_1"])) 
-#         assert(self.elements["ph264_1"].link(self.elements["mux_1"])) 
-#         self.log.debug("link mp4mux")     
-#         assert(self.elements["mux_1"].link(self.elements["filesink1"]))  
+        self.elements["recBin2"].add(self.elements["fileQueue2"])
+        self.elements["recBin2"].add(self.elements["ph264_2"])
+        self.elements["recBin2"].add(self.elements["mux_2"])
+        self.elements["recBin2"].add(self.elements["filesink2"])
         
-#         assert(self.elements["fakesrc"].link(self.elements["fileQueue2"]))
-#         assert(self.elements["fileQueue2"].link(self.elements["ph264_2"]))
-#         assert(self.elements["ph264_2"].link(self.elements["mux_2"]))
-#         assert(self.elements["mux_2"].link(self.elements["filesink2"]))
+        assert(self.elements["fileQueue2"].link(self.elements["ph264_2"])) 
+        assert(self.elements["ph264_2"].link(self.elements["mux_2"])) 
+        assert(self.elements["mux_2"].link(self.elements["filesink2"]))
+        
+        self.elements["recBin2"].add_pad(
+                                Gst.GhostPad.new("sink",
+                                self.elements["fileQueue2"].get_static_pad("sink")))
+        
+        
+#         self.elements["fakesrc"].set_property("sizemax", 1)
+#         
+#         self.pipelines["catch"].add(self.elements["fakesrc"])
+#         self.pipelines["catch"].add(self.elements["recBin2"])    
+#         assert(self.elements["fakesrc"].link(self.elements["recBin2"]))
+#         self.pipelines["catch"].set_state(Gst.State.PAUSED) 
         
         
         self.log.debug("set filesink location")
         self.elements["src"].set_property("location", "/run/media/peter/home/tmp/webcam/Video 66.mp4")
-        self.elements["filesink1"].set_property("location", "/run/media/peter/home/tmp/webcam/test1.mp4")
-        self.elements["filesink2"].set_property("location", "/run/media/peter/home/tmp/webcam/test2.mp4")
+        self.elements["filesink1"].set_property("location", "/run/media/peter/home/tmp/webcam/test0.mp4")
+#         self.elements["filesink2"].set_property("location", "/run/media/peter/home/tmp/webcam/test2.mp4")
                 
         
         self.log.debug("done")
         
     def run(self):        
         self.log.debug("start pipeline")
+        self.cnt = 0
         self.window.show_all()
         # You need to get the XID after window.show_all().  You shouldn't get it
         # in the on_sync_message() handler because threading issues will cause
@@ -234,84 +239,61 @@ class pipelineSwap(object):
 def preparePipeline(pad, probeInfo, userData):
     self = userData
     self.log.debug("pad blocked")
+    
     # remove padProbe
     pad.remove_probe(probeInfo.id)
     self.padProbes = [[pad, probeInfo]]
     
 ################################################################################
-    self.pipelines["main"].remove(self.elements["recBin1"])   
-#     
-# #     self.elements["recBin1"].set_state(Gst.State.PAUSED)
-    self.pipelines["catch"].set_state(Gst.State.PAUSED)
- 
-    self.elements["fakesrc"].unlink(self.elements["fakesink"])
-#     self.elements["fakesink"].set_state(Gst.State.NULL)
-    self.pipelines["catch"].remove(self.elements["fakesink"])
-    self.pipelines["catch"].add(self.elements["recBin1"])
-    assert(self.elements["fakesrc"].link(self.elements["recBin1"]))
-    self.pipelines["catch"].set_state(Gst.State.PLAYING)
-    Gst.debug_bin_to_dot_file_with_ts(self.pipelines["catch"], Gst.DebugGraphDetails.ALL, "catch" )
-    Gst.debug_bin_to_dot_file_with_ts(self.pipelines["main"], Gst.DebugGraphDetails.ALL, "main" )
-     
-#     self.pipelines["catch"].set_state(Gst.State.PLAYING) 
-    #self.pipeline2Null(self.pipelines["catch"])
+
+    self.cnt += 1
+    # get pad of current recBin
+    pC = pad.get_peer()
+    # get current recBin (the one that is currently doing the recording)
+    binC = pC.get_parent()
     
+    # get next recBin (the one that will be linked to the video stream next)
+    binN = None    
+    if binC == self.elements["recBin1"]:
+        self.log.debug("replace recBin1 by recBin2")
+        binN = self.elements["recBin2"]
+    else:
+        self.log.debug("replace recBin2 by recBin1")
+        binN = self.elements["recBin1"]
     
-    self.elements["recBin1"].send_event(Gst.Event.new_eos())  
+    self.log.debug("remove current recBin from main and prepare catch")
+    self.pipelines["main"].remove(binC)   
+#     self.pipelines["catch"].set_state(Gst.State.PAUSED) 
+# #     self.pipelines["catch"].remove(self.elements["fakesink"])
 #     
-#     self.log.debug("change file location")
-#     self.elements["filesink1"].set_property("location", "/run/media/peter/home/tmp/webcam/test3.mp4")
-#  
-#     self.log.debug("set catch to playing again")
+#     self.log.debug("add/link current recBin to catch")
+#     self.pipelines["catch"].add(self.elements["recBin1"])    
+#     assert(self.elements["fakesrc"].link(self.elements["recBin1"]))
+#     
+#     self.log.debug("set catch PLAYING")
 #     self.pipelines["catch"].set_state(Gst.State.PLAYING)
-#      
-#     self.log.debug("removing recBin1 from catch")
-#     self.pipelines["catch"].remove(self.elements["recBin1"])
-#  
-#     self.log.debug("adding recBin1 to main")
-#     self.pipelines["main"].add(self.elements["recBin1"])
-#     self.log.debug("linking")
-#     assert(self.pads["srcQueue"].link(self.elements["recBin1"]))
+    Gst.debug_bin_to_dot_file_with_ts(self.pipelines["catch"], Gst.DebugGraphDetails.ALL, "catch" )
+    Gst.debug_bin_to_dot_file_with_ts(self.pipelines["main"], Gst.DebugGraphDetails.ALL, "main" )
+         
+    self.log.debug("send EOS to current recBin")
+    binC.send_event(Gst.Event.new_eos())  
+#     
+    self.log.debug("prepare next recBin")    
+    binN.set_state(Gst.State.NULL)
+    self.log.debug("add and link next recBin to main")
+    self.pipelines["main"].add(binN)   
+    self.elements["srcQueue"].link(binN)  
     
-    self.elements["recBin2"].add(self.elements["fileQueue2"])
-    self.elements["recBin2"].add(self.elements["ph264_2"])
-    self.elements["recBin2"].add(self.elements["mux_2"])
-    self.elements["recBin2"].add(self.elements["filesink2"])
+    self.log.debug("change file location of next recBin")
+    fs = binN.get_by_name("filesink")
+    fs.set_property("location", "/run/media/peter/home/tmp/webcam/test{0}.mp4".format(self.cnt))
+    binN.set_state(Gst.State.PLAYING)
     
-    assert(self.elements["fileQueue2"].link(self.elements["ph264_2"])) 
-    assert(self.elements["ph264_2"].link(self.elements["mux_2"])) 
-    assert(self.elements["mux_2"].link(self.elements["filesink2"]))
-    
-    self.elements["recBin2"].add_pad(
-                            Gst.GhostPad.new("sink",
-                            self.elements["fileQueue2"].get_static_pad("sink")))
-    
-    self.pipelines["main"].add(self.elements["recBin2"])
-    
-    
-    self.log.debug("change file location")
-    self.elements["filesink2"].set_property("location", "/run/media/peter/home/tmp/webcam/test3.mp4")
-    
-    
-    self.elements["srcQueue"].link(self.elements["recBin2"])  
-    self.elements["recBin2"].set_state(Gst.State.PLAYING)
-    
+################################################################################
     
     
     Gst.debug_bin_to_dot_file_with_ts(self.pipelines["catch"], Gst.DebugGraphDetails.ALL, "catch" )
     Gst.debug_bin_to_dot_file_with_ts(self.pipelines["main"], Gst.DebugGraphDetails.ALL, "main" )
-    
-    ########################################################################### TODO: blockpad has to be a source
-    
-#     # install new probe for EOS
-    self.elements["filesink1"].get_static_pad("sink").add_probe(Gst.PadProbeType.EVENT_DOWNSTREAM, relinkPipeline, self)
-     
-    # send EOS
-#     self.cnt = 0 
-#     pad.get_peer().send_event(Gst.Event.new_eos())   
-#     Gst.debug_bin_to_dot_file_with_ts(self.pipelines["main"], Gst.DebugGraphDetails.ALL, "main_blocked" )
-    
-    
     
     return Gst.PadProbeReturn.OK
     
@@ -330,38 +312,14 @@ def relinkPipeline(pad, probeInfo, userData):
     
     self.padProbes += [[pad, probeInfo]]
     
-    
-         
-#     self.elements["recBin1"].send_event(Gst.Event.new_eos())  
-#     bus = self.elements["recBin1"].get_bus()        
-#     msg = bus.timed_pop_filtered (Gst.CLOCK_TIME_NONE, Gst.MessageType.EOS | Gst.MessageType.ERROR)
-#      
-#     if msg.type == Gst.MessageType.ERROR:
-#         self.log.debug("bus message {0}".format(msg.parse_error()))
-#         Gst.debug_bin_to_dot_file_with_ts(self.pipelines["main"], Gst.DebugGraphDetails.ALL, "main" )
-#         Gst.debug_bin_to_dot_file_with_ts(self.pipelines["catch"], Gst.DebugGraphDetails.ALL, "catch" )
-#      
     self.elements["filesink1"].set_state(Gst.State.NULL) 
     self.elements["filesink1"].set_property("location", "/run/media/peter/home/tmp/webcam/test3.mp4")    
     self.elements["filesink1"].set_state(Gst.State.PLAYING) 
     
     Gst.debug_bin_to_dot_file_with_ts(self.pipelines["main"], Gst.DebugGraphDetails.ALL, "main_blocked" )
     
-    
-#     for [p, pInfo] in self.padProbes:
-#         p.remove_probe(pInfo.id)        
-    
     return Gst.PadProbeReturn.OK
     
-#     self.log.debug("swapping pipeline")
-#     # active queue pad
-#     aqp = self.elements["fileQueue1"].get_static_pad("sink")
-#     # passive queue pad
-#     pqp = self.elements["fileQueue2"].get_static_pad("sink")
-#     # tee pad
-#     tp = aqp.get_peer()
-#     # fakesrc pad
-#     fp = pqp.get_peer()
         
         
 
