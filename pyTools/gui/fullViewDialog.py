2d8057d80d2c53b224233a0531dea3b8b7af7d05
Peter Rennert <p.rennert@cs.ucl.ac.uk>
2014-09-25 20:11:41 +0100

    added buttons in fullViewDialog

--- pyTools/gui/fullViewDialog.py
+++ pyTools/gui/fullViewDialog.py
@@ -1,4 +1,4 @@
-from PySide import QtCore, QtGui
+from PySide import QtCore, QtGui, QtSvg
 from pyTools.gui import fullViewDialog_auto
 from pyTools.gui.fullViewDialog_auto import Ui_Dialog
 import pyTools.videoTagger.hud as HUD
@@ -25,6 +25,9 @@ class FullViewDialog(QtGui.QMainWindow):
         self.scene = None
         self.hud = HUD.HUD(self.graphicsView)
         self.setupHUD()
+        self.setupControlWidget()
+        self.playing = False
+        self.editing = True
         self.mouseFilter = MouseFilterObj(self)
         self.installEventFilter(self.mouseFilter)
         # self.hud.installEventFilter(self.mouseFilter)
@@ -49,6 +52,78 @@ class FullViewDialog(QtGui.QMainWindow):
         self.hud.setColor(QtGui.QColor(255,255,255,50))
         self.hud.setGeometry(QtCore.QRect(10, 10, 841, 50))
 
+    def setupControlWidget(self):
+        self.controlWidget = QtGui.QWidget(self)
+        layout = QtGui.QHBoxLayout(self.controlWidget)
+
+        self.fdvButton = SVGButton(self.controlWidget)
+        self.fdvButton.load('../icon/Bar_chart_font_awesome.svg')
+        self.fdvButton.setToolTip("Open hierarchical view of labels")
+        self.fdvButton.setFixedSize(20, 20)
+        self.fdvButton.clicked.connect(self.parent().openFDV)
+        layout.addWidget(self.fdvButton)
+
+        self.timelineButton = SVGButton(self.controlWidget)
+        self.timelineButton.load('../icon/Align_justify_font_awesome.svg')
+        self.timelineButton.setToolTip("Open timeline panel (not implemented yet)")
+        self.timelineButton.setFixedSize(20, 20)
+        layout.addWidget(self.timelineButton)
+
+        layout.addSpacing(40)
+        layout.addStretch()
+
+        self.fullResButton = SVGButton(self.controlWidget)
+        self.fullResButton.load('../icon/Search_font_awesome.svg')
+        self.fullResButton.setToolTip("Show current frame in full resolution [SPACE]")
+        self.fullResButton.setFixedSize(20, 20)
+        self.fullResButton.clicked.connect(self.parent().displayFullResolutionFrame)
+        layout.addWidget(self.fullResButton)
+
+        layout.addSpacing(30)
+
+        self.playButton = SVGButton(self.controlWidget)
+        self.playButton.load('../icon/Play_font_awesome.svg')
+        self.playButton.setToolTip("Play video")
+        self.playButton.setFixedSize(20, 20)
+        self.playButton.clicked.connect(self.playButtonClick)
+        layout.addWidget(self.playButton)
+
+        layout.addSpacing(30)
+
+        self.modeButton = SVGButton(self.controlWidget)
+        self.modeButton.load('../icon/Edit_font_awesome.svg')
+        self.modeButton.setToolTip("Switch to 'Edit-mode' [CTRL + RETURN]")
+        self.modeButton.setFixedSize(20, 20)
+        self.modeButton.clicked.connect(self.toggleEditModeCheckbox)
+        layout.addWidget(self.modeButton)
+
+        layout.addStretch()
+
+        self.saveButton = SVGButton(self.controlWidget)
+        self.saveButton.load('../icon/Save_font_awesome.svg')
+        self.saveButton.setToolTip("Save all annotations [CTRL + S]")
+        self.saveButton.setFixedSize(20, 20)
+        self.saveButton.clicked.connect(self.parent().saveAll)
+        layout.addWidget(self.saveButton)
+
+        self.settingsButton = SVGButton(self.controlWidget)
+        self.settingsButton.load('../icon/Cogs_font_awesome.svg')
+        self.settingsButton.setToolTip("Open settings")
+        self.settingsButton.setFixedSize(20, 20)
+        self.settingsButton.clicked.connect(self.parent().openKeySettings)
+        layout.addWidget(self.settingsButton)
+
+        self.startVideoButton = SVGButton(self.controlWidget)
+        self.startVideoButton.load('../icon/Refresh_font_awesome.svg')
+        self.startVideoButton.setToolTip("Restart video event loop")
+        self.startVideoButton.setFixedSize(20, 20)
+        self.startVideoButton.clicked.connect(self.parent().startVideo)
+        layout.addWidget(self.startVideoButton)
+
+
+        self.splitter.addWidget(self.controlWidget)
+
+
     def setScene(self, scene):
         self.scene = scene
         self.graphicsView.setScene(self.scene)
@@ -57,8 +132,11 @@ class FullViewDialog(QtGui.QMainWindow):
 
     def resizeEvent(self, event):
         super(FullViewDialog, self).resizeEvent(event)
-        if self.scene:
-            self.graphicsView.fitInView(self.scene.sceneRect())
+        self.wasResized = True
+
+        bounds = self.graphicsView.scene().sceneRect()
+        self.graphicsView.fitInView(bounds, QtCore.Qt.KeepAspectRatio)
+        self.graphicsView.centerOn(0, 0)
 
     def showEvent(self, event):
         super(FullViewDialog, self).showEvent(event)
@@ -83,6 +161,67 @@ class FullViewDialog(QtGui.QMainWindow):
 
     def setMode(self, str, color=None):
         self.hud.setMode(str, color)
+
+    def playButtonClick(self):
+        if not self.playing:
+            self.playButton.load('../icon/Pause_font_awesome.svg')
+            self.playButton.setToolTip("Pause video")
+            self.parent().playback()
+        else:
+            self.playButton.load('../icon/Play_font_awesome.svg')
+            self.playButton.setToolTip("Play video")
+            self.parent().stopPlayback()
+
+        self.playing = not self.playing
+
+    def toggleEditModeCheckbox(self):
+        self.editing = not self.editing
+
+        if self.editing:
+            self.modeButton.load('../icon/Edit_font_awesome.svg')
+            self.modeButton.setToolTip("Switch to 'Edit-mode' [CTRL + RETURN]")
+        else:
+            self.modeButton.load('../icon/Plus_font_awesome.svg')
+            self.modeButton.setToolTip("Switch to 'Additive-mode' [CTRL + RETURN]")
+
+        self.parent().toggleEditModeCheckbox()
+
+
+
+
+
+class SVGButton(QtGui.QPushButton):
+
+    def __init__(self, svgPath=None, *args, **kwargs):
+        super(SVGButton, self).__init__(*args, **kwargs)
+        self.setSizePolicy(QtGui.QSizePolicy.Preferred,QtGui.QSizePolicy.Preferred)
+        self.icon = None
+
+        self.centralWidget = QtGui.QWidget(self)
+        self.setFlat(True)
+        self.setContentsMargins(0,0,0,0)
+
+        if svgPath is not None:
+            self.load(svgPath)
+
+
+    def load(self, svgPath):
+        if self.icon is None:
+            self.icon = QtSvg.QSvgWidget(svgPath, self.centralWidget)
+            self.icon.setFixedSize(self.size())
+        else:
+            self.icon.load(svgPath)
+
+        self.layoutBase = QtGui.QHBoxLayout(self)
+        self.layoutBase.addWidget(self.icon)
+
+    def resizeEvent(self, event):
+        super(SVGButton, self).resizeEvent(event)
+
+        if self.icon is not None:
+            self.icon.setFixedSize(self.size())
+
+
 
 
 class MouseFilterObj(QtCore.QObject):
