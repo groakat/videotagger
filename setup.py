fabe7519cc8c22666e9bd6603eea60def6487237
Peter Rennert <p.rennert@cs.ucl.ac.uk>
2014-10-15 18:11:10 +0100

    making setup.py installing videoTagger

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from __future__ import with_statement
 import os
 from setuptools import setup
 
@@ -7,6 +8,54 @@ from setuptools import setup
 # string in below ...
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+## windows install part from http://matthew-brett.github.io/pydagogue/installing_scripts.html
+import os
+from os.path import join as pjoin, splitext, split as psplit
+from distutils.core import setup
+from distutils.command.install_scripts import install_scripts
+from distutils import log
+
+BAT_TEMPLATE = \
+r"""@echo off
+set mypath=%~dp0
+set pyscript="%mypath%{FNAME}"
+set /p line1=<%pyscript%
+if "%line1:~0,2%" == "#!" (goto :goodstart)
+echo First line of %pyscript% does not start with "#!"
+exit /b 1
+:goodstart
+set py_exe=%line1:~2%
+call %py_exe% %pyscript% %*
+"""
+
+
+class my_install_scripts(install_scripts):
+    def run(self):
+        install_scripts.run(self)
+        if not os.name == "nt":
+            return
+        for filepath in self.get_outputs():
+            # If we can find an executable name in the #! top line of the script
+            # file, make .bat wrapper for script.
+            with open(filepath, 'rt') as fobj:
+                first_line = fobj.readline()
+            if not (first_line.startswith('#!') and
+                    'python' in first_line.lower()):
+                log.info("No #!python executable found, skipping .bat "
+                            "wrapper")
+                continue
+            pth, fname = psplit(filepath)
+            froot, ext = splitext(fname)
+            bat_file = pjoin(pth, froot + '.bat')
+            bat_contents = BAT_TEMPLATE.replace('{FNAME}', fname)
+            log.info("Making %s wrapper for %s" % (bat_file, filepath))
+            if self.dry_run:
+                continue
+            with open(bat_file, 'wt') as fobj:
+                fobj.write(bat_contents)
+
+### end windows install part
 
 setup(
     name = "pyTools",
@@ -23,4 +72,5 @@ setup(
         "Development Status :: 4 - Beta",
         "Topic :: Utilities",
     ],
+    cmdclass = {'install_scripts': my_install_scripts}
 )
