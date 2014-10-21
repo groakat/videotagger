6c32a8a17edfa72aca62b1cf61420c0d0e00b181
Peter Rennert <p.rennert@cs.ucl.ac.uk>
2014-10-21 17:21:05 +0100

    fixing enter-bug in autocompletecombobox

--- pyTools/videoTagger/modifyableRect.py
+++ pyTools/videoTagger/modifyableRect.py
@@ -32,6 +32,12 @@ class Test(QtGui.QMainWindow):
         MainWindow.setCentralWidget(self.centralwidget)
 
 
+        self.le = AutoCompleteComboBox(self.centralwidget)
+        self.le.setGeometry(QtCore.QRect(100, 500, 94, 69))
+        self.le.setModel(['ok', 'computer', 'mutter', 'nothin'])
+
+
+
 
     def connectElements(self):
         self.pb_debug.clicked.connect(self.buttonClick)
@@ -139,13 +145,13 @@ class CustomQCompleter(QtGui.QCompleter):
 
         self.filterProxyModel.setFilterRegExp(pattern)
 
-
     def splitPath(self, path):
         self.local_completion_prefix = path
         self.updateModel()
         if self.filterProxyModel.rowCount() == 0:
             self.usingOriginalModel = False
             self.filterProxyModel.setSourceModel(QtGui.QStringListModel([path]))
+            return [path]
 
         return []
 
@@ -172,15 +178,12 @@ class AutoCompleteComboBox(QtGui.QComboBox):
         self.setEditable(True)
         self.setInsertPolicy(self.NoInsert)
 
-        # self.comp = QtGui.QCompleter([""], self)
         self.comp = CustomQCompleter(self)
         self.comp.setCompletionMode(QtGui.QCompleter.PopupCompletion)
-        # self.comp.setCaseSensitivity(QtCore.Qt.CaseInsensitive)
         self.setCompleter(self.comp)#
-        self.setModel(["hallo babe", "world", "we", "are good"])
+        self.setModel(["Lola", "Lila", "Cola", 'Lothian'])
 
     def setModel(self, strList):
-        # self.comp.model().setStringList(strList)
         self.clear()
         self.insertItems(0, strList)
         self.comp.setModel(self.model())
@@ -188,6 +191,21 @@ class AutoCompleteComboBox(QtGui.QComboBox):
     def focusInEvent(self, event):
         self.clearEditText()
         super(AutoCompleteComboBox, self).focusInEvent(event)
+
+    def keyPressEvent(self, event):
+        key = event.key()
+        if key == 16777220:
+            # Enter (if event.key() == QtCore.Qt.Key_Enter) does not work
+            # for some reason
+
+            # make sure that the completer does not set the
+            # currentText of the combobox to "" when pressing enter
+            text = self.currentText()
+            self.setCompleter(None)
+            self.setEditText(text)
+            self.setCompleter(self.comp)
+
+        return super(AutoCompleteComboBox, self).keyPressEvent(event)
 
 
 
