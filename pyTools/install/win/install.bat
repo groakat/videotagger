70b6e236f31ce504129b775712eed53bca367238
Peter Rennert <p.rennert@cs.ucl.ac.uk>
2015-04-23 08:31:04 +0100

    fixing bugs in creating shortcut for videotagger

--- pyTools/install/win/install.bat
+++ pyTools/install/win/install.bat
@@ -3,7 +3,7 @@ conda install -y videotagger
 
 REM install simple bat call
 
-copy %USERPROFILE%\Anaconda\Lib\site-packages\pyTools\install\win\videotagger.bat %USERPROFILE%\Anaconda\Scripts\videotagger.bat
+copy %USERPROFILE%\Anaconda\Lib\site-packages\pyTools-0.1.0-py2.7.egg\pyTools\install\win\videotagger.bat %USERPROFILE%\Anaconda\Scripts\
 
 REM create shortcut
 
@@ -14,9 +14,11 @@ set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
 echo sLinkFile = "%USERPROFILE%\Desktop\VideoTagger.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
-echo oLink.TargetPath = "videotagger.bat" >> %SCRIPT%
-echo oLink.IconLocation = "%USERPROFILE%\Anaconda\Lib\site-packages\pyTools\icon\3d186778-48ba-11e4-8ece-d88fa9171785.ico" >> %SCRIPT%
+echo oLink.TargetPath = "%USERPROFILE%\Anaconda\Scripts\videotagger.bat" >> %SCRIPT%
+echo oLink.IconLocation = "%USERPROFILE%\Anaconda\Lib\site-packages\pyTools-0.1.0-py2.7.egg\pyTools\icon\3d186778-48ba-11e4-8ece-d88fa9171785.ico" >> %SCRIPT%
 echo oLink.Save >> %SCRIPT%
 
 cscript /nologo %SCRIPT%
 del %SCRIPT%
+
+pause
