5d08a9b80c8c7f52fa492f2fb5e0935455dec5e2
Peter Rennert <p.rennert@cs.ucl.ac.uk>
2015-06-08 21:31:52 +0100

    using libx264, fixing aspect ratio issues, fixing height and width issues

--- pyTools/videoTagger/prepareFolderForVideoProcessing.py
+++ pyTools/videoTagger/prepareFolderForVideoProcessing.py
@@ -28,18 +28,43 @@ def checkForCleanFolder(folder):
     else:
         raise ValueError("The directory you have chosen is not 'clean'. I.e. you need to select a folder that contains only a single video file you want to work on")
 
-def generateSmallVideo(videoPath, ext='avi'):
+def generateSmallVideo(videoPath, ext='mp4'):
     basename = os.path.basename(videoPath)
     # ext = basename.split(os.path.extsep)[-1]
     basename_wo_ext = os.path.extsep.join(basename.split(os.path.extsep)[:-1])
     folder = os.path.dirname(videoPath)
 
+    p = subprocess.Popen("ffprobe -v error -show_entries stream=width,height,avg_frame_rate " +
+                         "-of default=noprint_wrappers=1:nokey=1 {}".format(videoPath),
+                         shell=True,  stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    output = p.communicate()[0]
+    width, height, fps = output.split()
+
+    width = int(width)
+    height = int(height)
+
+    if '/' in fps:
+        a, b = [float(x) for x in fps.split('/')]
+        fps = a / b
+    else:
+        fps = float(fps)
+
+    low_res_w = 640
+    down_ratio = float(low_res_w) / width
+    low_res_h = int(down_ratio * height)
+
+    if low_res_h % 2 != 0:
+        low_res_h += 1
+
     targetBasename = basename_wo_ext + "_small" + os.path.extsep + ext
     targetPath = os.path.join(folder, targetBasename)
 
-    ffmpegStr = ('ffmpeg -i "{orgPath}" -y -c:v ffv1 -vf scale=320:-1 ' + \
-                '-qscale 0 -r 30 "{targetPath}"').format(orgPath=videoPath,
-                                                 targetPath=targetPath)
+    ffmpegStr = ('ffmpeg -i "{orgPath}" -y -c:v libx264 -crf 18 -g 1 -vf scale={low_res_w}:{low_res_h} ' + \
+                ' "{targetPath}"').format(orgPath=videoPath,
+                                               targetPath=targetPath,
+                                               low_res_w=low_res_w,
+                                               low_res_h=low_res_h)
 
     print ffmpegStr
 
@@ -51,9 +76,26 @@ def generateSmallVideo(videoPath, ext='avi'):
     targetFullBasename = basename_wo_ext + "_full" + os.path.extsep + ext
     targetFullPath = os.path.join(folder, targetFullBasename)
 
-    ffmpegStr = ('ffmpeg -i "{orgPath}" -y -an ' +\
-                '-qscale 0 -c:v ffv1 -r 30 "{targetPath}"').format(orgPath=videoPath,
-                                                      targetPath=targetFullPath)
+    rescale = False
+    if height % 2 != 0:
+        height += 1
+        rescale = True
+
+    if width % 2 != 0:
+        width += 1
+        rescale = True
+
+    if not rescale:
+        ffmpegStr = ('ffmpeg -i "{orgPath}" -y -an ' +\
+                    ' -c:v libx264 -crf 18 -g 1 "{targetPath}"').format(orgPath=videoPath,
+                                                          targetPath=targetFullPath)
+    else:
+        ffmpegStr = ('ffmpeg -i "{orgPath}" -y -an ' +\
+                    ' -c:v libx264 -crf 18 -g 1 -vf scale={width}:{height} "{targetPath}"').format(
+                                                    orgPath=videoPath,
+                                                    targetPath=targetFullPath,
+                                                    width=width,
+                                                    height=height)
 
     p = subprocess.Popen(ffmpegStr, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     output = p.communicate()[0]
